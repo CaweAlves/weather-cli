455c583c535f30dd7bdcc562003fcb52561b4333
Cawe Alves <cawe.wewe04@gmail.com>
2023-08-16 20:42:59 -0300

    update(default command): alter to return four days weather

--- app/Command/Weather/DefaultController.php
+++ app/Command/Weather/DefaultController.php
@@ -2,12 +2,28 @@
 
 namespace App\Command\Weather;
 
+use App\Integration\Cptec;
 use Minicli\Command\CommandController;
 
 class DefaultController extends CommandController
 {
+
+    private string $urn = 'cidade/codigo_da_localidade/previsao.xml';
+    private string $cityCode;
+
+    public function make(): void
+    {
+        $this->cityCode = $this->getParam('cityCode');
+        $this->urn = str_replace('codigo_da_localidade', $this->cityCode, $this->urn);
+    }
+
     public function handle(): void
     {
-        $this->display("weather command controller");
+        $this->make();
+        $client = new Cptec($this->urn);
+
+        $weather = $client->getWeather();
+        var_dump($weather);
+        $this->display("capitals weather command controller");
     }
 }
